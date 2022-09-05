02f3e304a93f284d0441bac9e59b9e480a12be33
LetsKaiou <95400481+LetsKaiou@users.noreply.github.com>
2022-09-05 16:22:25 +0900

    Revert "Revert "Revert "Revert "しょｔｔｔっと""""

--- Ta-mya/Assets/MyFolder/Scripts/Player.cs
+++ Ta-mya/Assets/MyFolder/Scripts/Player.cs
@@ -27,15 +27,16 @@ public class Player : MonoBehaviourPunCallbacks
 
     // ゲーム実行中の繰り返し処理
     void Update()
-    {        
+    {
+
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(new Vector3(0, RoteSpeed, 0) * Time.deltaTime);
+            transform.position += transform.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(new Vector3(0, -RoteSpeed, 0) * Time.deltaTime);
+            transform.position -= transform.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.W))
         {
@@ -55,27 +56,27 @@ public class Player : MonoBehaviourPunCallbacks
         }
         if (Input.GetMouseButtonDown(0))
         {
-            // 弾の生成
-            //Bullet.instance.shot();
-            //Instantiate(Bullet, transform.position + bulletPoint, Quaternion.identity);
-
-            //弾を出現させる位置を取得
-            Vector3 placePosition = this.transform.position;
-            //出現させる位置をずらす値
-            Vector3 offsetGun = new Vector3(0, 0, 2);
-
-            //武器の向きに合わせて弾の向きも調整
-            Quaternion q1 = this.transform.rotation;
-            //弾を90度回転させる処理
-            Quaternion q2 = Quaternion.AngleAxis(90, new Vector3(1, 0, 0));
-            Quaternion q = q1 * q2;
-
-            //弾を出現させる位置を調整
-            placePosition = q1 * offsetGun + placePosition;
-            //弾生成！
-            Instantiate(bullet, placePosition, q);
-
+            shot();
         }
+    }
+
+    public void shot()
+    {
+        //弾を出現させる位置を取得
+        Vector3 placePosition = this.transform.position;
+        //出現させる位置をずらす値
+        Vector3 offsetGun = new Vector3(0, 0, 2);
+
+        //武器の向きに合わせて弾の向きも調整
+        Quaternion q1 = this.transform.rotation;
+        //弾を90度回転させる処理
+        Quaternion q2 = Quaternion.AngleAxis(90, new Vector3(1, 0, 0));
+        Quaternion q = q1 * q2;
+
+        //弾を出現させる位置を調整
+        placePosition = q1 * offsetGun + placePosition;
+        //弾生成！
+        Instantiate(bullet, placePosition, Quaternion.identity);
     }
 }
 
