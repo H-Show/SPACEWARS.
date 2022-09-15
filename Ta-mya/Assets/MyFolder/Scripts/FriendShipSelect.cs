b3773146dbe0c3a49b5188b4d6e6c5820f972538
LetsKaiou <san-A20013@sist.ac.jp>
2022-09-15 16:14:32 +0900

    csv

--- Ta-mya/Assets/MyFolder/Scripts/FriendShipSelect.cs
+++ Ta-mya/Assets/MyFolder/Scripts/FriendShipSelect.cs
@@ -6,6 +6,7 @@ public class FriendShipSelect : MonoBehaviour
 {
     #region メモ
     // csvで読み込んできたデータを格納する変数の作成 〇
+    // どの機体が選択されたのかを調べる変数作成 〇
     // どの機体が選択されたのかを調べる
     // 読み込んできたデータをStart関数内で格納 〇
     // 選択された機体のデータを格納
@@ -14,17 +15,39 @@ public class FriendShipSelect : MonoBehaviour
     #endregion
 
     private FriendShipInfo statusInfo;
-    private int SelectNum;
-    
-    
+    private int count = 0;
+    [SerializeField] private int[] SelectNum = new int[3];
+    public string[] Name = new string[3];
+    public int[] HP = new int[3];
+    public int[] DEF = new int[3];
+    public int[] SPD = new int[3];
+
+
     void Start()
     {
+        count = 0;
         statusInfo = new FriendShipInfo();
         statusInfo.Init();
+        CreateShip();
     }
 
-    void Update()
+
+    private void CreateShip()
     {
-        
+        //while(count < 3)
+        //{
+            Name[count] = statusInfo.Name[SelectNum[count]];
+            HP[count] = statusInfo.HP[SelectNum[count]];
+            DEF[count] = statusInfo.DEF[SelectNum[count]];
+            SPD[count] = statusInfo.SPD[SelectNum[count]];
+
+            Debug.Log("機体1:" + Name[count]);
+            Debug.Log("機体1:" + HP[count]);
+            Debug.Log("機体1:" + DEF[count]);
+            Debug.Log("機体1:" + SPD[count]);
+
+            count++;
+
+        //}
     }
 }
