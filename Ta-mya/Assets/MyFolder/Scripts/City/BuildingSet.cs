cd8bf73a5f8226dbab05ec8e433c9d9782b032c9
H-Show <95389843+H-Show@users.noreply.github.com>
2022-10-07 16:32:36 +0900

    1007

--- Ta-mya/Assets/MyFolder/Scripts/City/BuildingSet.cs
+++ Ta-mya/Assets/MyFolder/Scripts/City/BuildingSet.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class BuildingSet : MonoBehaviour
 {
+    public GameObject obj;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject obj = GameObject.Find("Building");
-        GameObject newObj = Instantiate(obj);
-        newObj.transform.position = new Vector3(8, 0, 0);
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Instantiate( 生成するオブジェクト,  場所, 回転 );
+            Instantiate(obj, new Vector3(320.0f, 10.0f, 300.0f), Quaternion.identity);
+        }
     }
 }
